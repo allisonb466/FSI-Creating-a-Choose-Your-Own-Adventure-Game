493cf99e4f0f71d4b00feced585deae389a12868
allisonb466 <allisonbyrne466@gmail.com>
2022-07-24 17:43:30 -0400

    More Glitches

--- index.js
+++ index.js
@@ -3,23 +3,20 @@ let firstAnswer = window.prompt('You approach a fork in the road. Do you head Le
 if(firstAnswer === 'Left'){
     let secondAnswer = window.prompt(`You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it, or continue on your path? Type Follow or Continue.`);
 if(secondAnswer === 'Follow'){
-    let thirdAnswer = window.prompt(`You follow the cat to a colony of cats,
-nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
-They are content with you staying, but you wonder if you should alert the world 
-to this magical safe haven. Type Stay or Spread the word.`)
+    let thirdAnswer = window.prompt(`You follow the cat to a colony of cats,nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven. Type Stay or Spread the word.`)
 if(thirdAnswer === 'Stay'){
     let fourthanswer = window.alert ('You live happily amongst the cats for the rest of your days.')
 }else if(thirdAnswer === 'Spread the word'){
     let fourthanswer = window.alert ('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.'
 )}
 }else if(secondAnswer === 'Continue'){
-    let thirdAnswer = window.prompt ('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take? Type Ladder or Staircase.')};
+    let thirdAnswer = window.prompt ('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take? Type Ladder or Staircase.');
 if(thirdAnswer === 'Ladder'){
-        let fourthAnswer = window.alert ('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.'
+    let fourthAnswer = window.alert ('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.'
     )}
-    else if(thirdAnswer === 'Staircase'){
-        let fourthAnswer = window.alert ('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.'
-    )}
+else if(thirdAnswer === 'Staircase'){
+    let fourthAnswer = window.alert ('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.'
+    )}}
 }else if(firstAnswer === 'Right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take? Type Past the dragon or Away from the dragon.`)
 if (secondAnswer === 'Past the dragon'){
